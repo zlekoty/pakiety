4df28f9409b2dfa72f31027739857b91704d80da
martyna-B <82177486+martyna-B@users.noreply.github.com>
2021-04-29 12:59:04 +0200

    Nowy kod u Julii
    Kod jest bardziej ułożony, są też animacje od Łukasza (ale wymagają jeszcze poprawek pod względem estetycznym).

--- projekt.jl
+++ projekt.jl
@@ -1,22 +1,77 @@
 using Plots
 
-jelenie_pocz=20.0 # ilosc poczatkowa ofiar
-wilki_pocz=10.0 # ilosc poczatkowa drapieżników
-narodziny_jeleni=0.9 # współczynnik urodzeń ofiar
-szansa_upolowania=0.1 # szansa na upolowanie
-narodziny_wilkow=0.5  # współczynnik urodzeń drapieżników
-wsp_umier_wilkow=0.6 # współczynnik umieralnosci drapieżników
-
-J = collect(10.0 for i in 1:1000) #macierz jeleni
-J[1] = jelenie_pocz
-W = collect(10.0 for i in 1:1000) #macierz wilków
-W[1] = wilki_pocz
-
-
-for i in 2:1000
-    J[i] = J[i-1] + ((J[i-1]*narodziny_jeleni)-(szansa_upolowania*W[i-1]*J[i-1]))*0.04
-    W[i] = W[i-1] + ((szansa_upolowania*narodziny_wilkow*J[i-1]*W[i-1]) - wsp_umier_wilkow*W[i-1])*0.04
+function stan_początkowy_jeleni(jelenie_pocz)
+    """
+    Funkcja tworzy macierz, której pierwszym elementem jest początkowa ilość jeleni,
+    a kolejnymi zera. Kolejne elementy będą reprezentować ilość jeleni w danym czasie.
+    """
+    J = zeros(24999) 
+    J[1] = jelenie_pocz
+    return J
 end
-    
-plot(J, label="ilość jeleni", ylabel= "liczba osobników", xlabel = "czas")
-plot!(W, label = "ilość wilków")
+
+function stan_początkowy_wilków(wilki_pocz)
+    """
+    Funkcja tworzy macierz, której pierwszym elementem jest początkowa ilość wilków,
+    a kolejnymi zera. Kolejne elementy będą reprezentować ilość wilków w danym czasie.
+    """
+    W = zeros(24999)
+    W[1] = wilki_pocz
+    return W
+end
+
+J = stan_początkowy_jeleni(20.0)
+W = stan_początkowy_wilków(10.0)
+pojemność_środowiskowa = 500 # Pojemność środowiskowa dla jeleni. Im bliżej ich populacja zbliża się do tej liczby, tym wolniej się rozmnażają.
+narodziny_wilkow = 0.5  # Współczynnik urodzeń drapieżników.
+wsp_umier_wilkow = 0.6 # Współczynnik umieralnosci drapieżników.
+
+function zmiana(narodziny_jeleni, szansa_upolowania)
+    """
+    Funkcja modyfikuje kolejne elementy macierzy J i W tak, aby reprezentowały zmieniającą się liczbę osobników
+    populacji jeleni oraz wilków.
+    """
+    global pojemność_środowiskowa
+    global J
+    global W 
+    global narodziny_wilkow
+    global wsp_umier_wilkow
+    for i in 2:24999  
+        J[i] = J[i-1] + ((J[i-1]*narodziny_jeleni)-(szansa_upolowania*W[i-1]*J[i-1]))*(1-J[i-1]/pojemność_środowiskowa)*0.002
+        W[i] = W[i-1] + ((szansa_upolowania*narodziny_wilkow*J[i-1]*W[i-1]) - wsp_umier_wilkow*W[i-1])*0.002
+    end
+end
+
+function anim_narodziny_jeleni()
+    """
+    Funkcja tworzy animację zmieniających się populcji w zależności od współczynnika narodzin jeleni.
+    Oscyluje on między wartością 0.2 a 2.
+    """
+    global J
+    global W
+    an = @animate for k in 1:500 # k/250 jest współczynnikiem urodzin jeleni
+        zmiana(k/250, 0.05)
+        plot(J, title = "Wykres w zależności od współczynnika narodzin jeleni", label = "ilość jeleni", ylabel = "liczba osobników", xlabel = "czas")
+        plot!(W, label = "ilość wilków") 
+        J = stan_początkowy_jeleni(20.0)
+        W = stan_początkowy_wilków(10.0)
+    end
+    gif(an, fps = 10)
+end
+
+function anim_szansa_upolowania()
+    """
+    Funkcja tworzy animację zmieniających się populcji w zależności od szansy upolowania jelenia przez wilka.
+    Oscyluje on między wartością ~ 0.003 a ~ 0.49.
+    """
+    global J
+    global W
+    an = @animate for k in 1:500 # k/1000 jest szansą upolowania jelenia przez wilka
+        zmiana(0.9, k/1000)
+        plot(J, title = "Wykres w zależności od szansy upolowania", label = "ilość jeleni", ylabel = "liczba osobników", xlabel = "czas")
+        plot!(W, label = "ilość wilków") 
+        J = stan_początkowy_jeleni(20.0)
+        W = stan_początkowy_wilków(10.0)
+    end
+    gif(an, fps = 10)
+end
